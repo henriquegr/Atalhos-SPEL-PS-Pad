c402b9b9a8856a07ab3f01afd98620677c27927c
unknown <Administrador@WINXP.(none)>
2012-09-01 22:11:18 -0300

    Bats para deploys dos templates nas pastas corretas

---
+++ RESTORE_D.bat
@@ -0,0 +1,6 @@
+Rem Configurações específicas do PSPad
+xcopy "Z:\Home\temp\Tools\PSPad\*.ini" "%appdata%\PSPad"  /Y
+xcopy "Z:\Home\temp\Tools\PSPad\Context" "D:\Program Files\PSPad editor\Context"  /Y
+xcopy "Z:\Home\temp\Tools\PSPad\Syntax" "D:\Program Files\PSPad editor\Syntax" /Y
+
+pause
---
+++ restore_C.bat
@@ -0,0 +1,6 @@
+Rem Configurações específicas do PSPad
+xcopy "Z:\home\temp\Tools\windows\PSPad\*.ini" "%appdata%\PSPad"  /Y
+xcopy "Z:\home\temp\Tools\windows\PSPad\Context" "C:\Program Files\PSPad editor\Context"  /Y
+xcopy "Z:\home\temp\Tools\windows\PSPad\Syntax" "C:\Program Files\PSPad editor\Syntax" /Y
+
+pause
