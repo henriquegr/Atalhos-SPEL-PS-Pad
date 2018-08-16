4cc6f2e2640e588c4aa59567cb32e67a0df951ab
Henrique Grammelsbacher <henrique@mphg.com.br>
2018-08-16 14:19:29 -0300

    Reconhecimento de novas palavras chave e alteracao do padrao de sniplets spel

--- restore_C.bat
+++ restore_C.bat
@@ -1,5 +1,5 @@
 Rem Configurações específicas do PSPad
-xcopy "%~dp0\*.ini" "%appdata%\PSPad"  /Y
+xcopy "%~dp0*.ini" "%appdata%\PSPad"  /Y
 xcopy "%~dp0\Context" "C:\Program Files\PSPad editor\Context"  /Y
 xcopy "%~dp0\Syntax" "C:\Program Files\PSPad editor\Syntax" /Y
 
